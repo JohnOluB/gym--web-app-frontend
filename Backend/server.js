75e7d6bec841f60e3104be3a4e9b38c24426bc38
JohnOluB <adebayodeolu90@gmail.com>
2025-11-06 18:46:21 +0100

    feat: added server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,13 @@
-import express from express
+import express from "express";
 
 const app = express();
 
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.listen(3000, () => {
-console.log(`Server is running on ${PORT}`);
-}
+app.listen(3000, (req, res) => {
+  console.log(`Server is running on: http://localhost:${PORT}`);
+});
