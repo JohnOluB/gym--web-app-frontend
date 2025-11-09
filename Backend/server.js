e828e83a593a6a6fd935909f302ebc6e74a57f9b
JohnOluB <adebayodeolu90@gmail.com>
2025-11-09 01:04:33 +0100

    fix: comment endpoint

--- Backend/server.js
+++ Backend/server.js
@@ -4,9 +4,9 @@ const app = express();
 
 const PORT = 3000;
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(3000, (req, res) => {
   console.log(`Server is running on: http://localhost:${PORT}`);
